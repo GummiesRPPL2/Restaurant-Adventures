7d101cf2a96df4ba1059dd5af082f3a372cb6251
GummiesRPPL2 <137831177+GummiesRPPL2@users.noreply.github.com>
2024-05-06 13:14:50 -0500

    Restarting

--- RestaurantAdventures/Assets/Scripts/ScriptableObjects/PartySO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Party", menuName = "Parties/Party")]
-public class PartySO : ScriptableObject
-{ 
-    public List<CharacterSO> characters = new List<CharacterSO>();
-}
